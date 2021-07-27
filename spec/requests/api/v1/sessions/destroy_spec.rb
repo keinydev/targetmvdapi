481cf14669f020f75db8460573a4a586a805cfb3
keinydev <keinyp891@gmail.com>
2021-07-27 18:31:08 -0500

    Create destroy_spec.rb

---
+++ spec/requests/api/v1/sessions/destroy_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+RSpec.describe "DELETE api/v1/users/sign_out", type: :request do
+
+  let(:user) { create(:user) }
+  
+  let(:params) do {
+    user: {
+      email: user.email,
+      password: user.password
+    }    
+  }
+  end
+
+  before do
+    post new_user_session_path, params: params, as: :json
+  end
+ 
+  let(:headers) do {
+    'uid': response.headers['uid'],
+    'client': response.headers['client'],
+    'access-token': response.headers['access-token']
+  }
+  end
+
+  subject(:signout) { delete destroy_user_session_path, headers: headers }
+
+  describe 'DELETE Destroy' do
+    context 'when logout params are valid' do
+      before do
+        signout
+      end
+
+      it 'returns status 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the header is not correct' do
+      before do
+        headers[:uid] = ""
+        signout
+      end
+
+      it 'returns an error message' do
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response['errors']).to eq(['User was not found or was not logged in.']) 
+      end
+    end
+  end
+end
