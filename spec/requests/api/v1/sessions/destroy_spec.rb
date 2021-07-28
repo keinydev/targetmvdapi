d9600867b6d2df12a3c5e7cef132a648c2f77bf6
keinydev <keinyp891@gmail.com>
2021-07-28 12:39:32 -0500

    Update destroy_spec.rb

--- spec/requests/api/v1/sessions/destroy_spec.rb
+++ spec/requests/api/v1/sessions/destroy_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "DELETE api/v1/users/sign_out", type: :request do
+describe "DELETE api/v1/users/sign_out", type: :request do
 
   let(:user) { create(:user) }
   
