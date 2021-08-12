738fab9e8f1c0de4f97ee003833be677a8162379
keinydev <keinyp891@gmail.com>
2021-08-12 14:18:40 -0500

    Deleting a target

--- app/controllers/api/v1/targets_controller.rb
+++ app/controllers/api/v1/targets_controller.rb
@@ -3,6 +3,7 @@ module Api
     class TargetsController < ApplicationController
 
       before_action :authenticate_user!
+      before_action :set_target, only: :destroy
 
       def create
         @target = current_user.targets.build(target_params)
@@ -12,11 +13,20 @@ module Api
           render json: { errors: @target.errors }, status: :unprocessable_entity
         end
       end  
+
+      def destroy
+        @target.destroy
+        head :no_content
+      end 
       
       private
 
       def target_params
         params.require(:target).permit(:title, :radius, :latitude, :longitude, :topic_id)
+      end
+
+      def set_target
+        @target = Target.find(params[:id])
       end
     end
   end
---
+++ spec/requests/api/v1/targets/destroy_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+describe "DELETE api/v1/targets/:id", type: :request do
+
+  let(:topic_list) { create_list(:topic, 5) } 
+
+  let(:user) { create(:user) }
+
+  let(:target) { create(:target, user: user, topic: topic_list.first) }
+
+  subject(:delete_target) { delete api_v1_target_path(target.id), headers: auth_headers, as: :json }
+
+  describe 'DELETE Destroy' do
+    context 'when params are valid' do
+      before do
+        delete_target
+      end
+      
+      it 'returns status 204' do
+        expect(response).to be_successful
+      end    
+    end
+
+    context 'when the header is not correct' do
+      before do
+        auth_headers[:uid] = ""
+        delete_target
+      end
+
+      it 'returns an error message' do
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response['errors']).to eq(['You need to sign in or sign up before continuing.']) 
+      end
+    end
+  end
+end
