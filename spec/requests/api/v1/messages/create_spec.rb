43fa6ce5660905d88ec94b3cb96d5c11e56b7e7b
keinydev <keinyp891@gmail.com>
2021-08-27 14:22:05 -0500

    Update create_spec.rb

--- spec/requests/api/v1/messages/create_spec.rb
+++ spec/requests/api/v1/messages/create_spec.rb
@@ -17,27 +17,29 @@ describe "POST api/v1/conversations/:conversation_id/messages", type: :request d
 
   subject(:create_message) { post api_v1_conversation_messages_path(conversation_a.id), params: message_params, headers: auth_headers, as: :json }
 
-  describe 'POST Create' do
-    context 'when params are valid' do
-      before do
-        create_message
-      end
-      
-      it 'returns a successful response' do
-        expect(response).to be_successful
-      end     
+  context 'when params are valid' do
+    before do
+      create_message
+    end
+    
+    it 'returns a successful response' do
+      expect(response).to be_successful
+    end  
+
+    it 'create a message in the database' do
+      expect(Message.count).to eq(1)
+    end       
+  end
+
+  context 'when the header is not correct' do
+    before do
+      auth_headers[:uid] = ""
+      create_message
     end
 
-    context 'when the header is not correct' do
-      before do
-        auth_headers[:uid] = ""
-        create_message
-      end
-
-      it 'returns an error message' do
-        parsed_response = JSON.parse(response.body)
-        expect(parsed_response['errors']).to eq(['You need to sign in or sign up before continuing.']) 
-      end
+    it 'returns an error message' do
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response['errors']).to eq(['You need to sign in or sign up before continuing.']) 
     end
   end
 end
