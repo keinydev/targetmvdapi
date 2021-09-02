6c0f223755311a15589d8aa6ca97dded97b26588
keinydev <keinyp891@gmail.com>
2021-09-02 12:12:47 -0500

    Comments with annotate

--- spec/factories/benefits.rb
+++ spec/factories/benefits.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: benefits
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  price      :decimal(, )      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'faker'
 
 FactoryBot.define do
